11dbe6f79b9b09d7d6784d7a97cd45c3b24c5eda
Paulo Alzaga <pauloalzaga@Paulos-MacBook-Air.local>
2018-05-24 18:38:28 -0700

    first draft of html and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+var win = 0; 
+var loss = 0;
+var ranNum = x;
+var score = 0;
+
+  $("img").on("click", function(img){
+    console.log("you clicked a button");
+  })
+  
+// for(var i = 0; i < ; i++) {
+    // console.log(i);
+  //}
+
+  // var ranNum = word[Math.floor(Math.random() * word.length)];
+  
+
+
+
