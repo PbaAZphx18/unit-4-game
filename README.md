5d0ab3616455f890376ae20e3229743cc98a90e4
Paulo Alzaga <pauloalzaga@Paulos-MacBook-Air.local>
2018-05-31 14:18:24 -0700

    updating js file to submit homework

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # unit-4-game
+Crystal Collector game
+Object of game is to match your score to the random number generated at start.
+User has 4 crystals to choose from, each of which contains a random number between 1-12.
+User can then press each crystal as many times as needed, in order to match the random number generated at the start that falls between 19-120.
+If user matches score, game logs a win.  If player goes over, game logs a loss.
+Game resets after each win or loss.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,152 @@
+//-------Get writers block in that I dont know what to do first or how things go together. 
+//-------I still have a hard time figuring out what functions/methods to use when and where----
+
+
 
 
 // $(document).ready(function()-------------------
 
+
+// function newGame() {
+  // total = 0;
+  // $(".paraWin").text(total);
+  // compNum = Math.floor(Math.random() * 120) + 19;
+  // console.log(compNum);
+  // $(".paraRandom").text(compNum);
+
+//   for (var i = 0; i < 4; i++) {
+//     crystals[i] = Math.floor(Math.random() * 12) + 1;
+//   }
+//   console.log(crystals);
+// }
+
+// function playGame() {
+//   if (total === compNum) {
+//     win = win + 1;
+//     console.log("wins are " + win);
+//   }
+    // }
+
+
+
+
+
+
+
+
+
+
+// -------------
+
+// compNum = 0;
+// $(".finalScore").text(sum);
+// // Computer assigns a random number between 19 and 120 inclusive that the user will try to match
+// Var ranNum = Math.floor(Math.random() * 120 + 19);
+// console.log(ranNum);
+// $(".paraRandom").text(ranNum);
+
+
+// // Computer assigns different random numbers between 1 and 12 inclusive to each of the crystals 
+// for (var i=0; i<4; i++) {
+//     crystals[i] = Math.floor(Math.random() * 12 + 1);
+// }
+// console.log(crystals);
+
+
+// }
+//------------------------
+
 //variables
-var win = 0;
-var loss = 0;
-var compNum = 1;
-var score = 0;
-var ranNum1 = [0,0,0,0];
-var min = 19;
-var max = 120;
+var wins = 0;
+var losses = 0;
+var compNum = 0;
+var userScore = 0;
+var total = 0;
 
-
-// Chooses a new random number 
-  var ranNum = Math.floor(Math.random() * 120 + 19);
+// Chooses a new random number at start of new game
+  var ranNum = 19 + Math.floor(Math.random() * 102);
   console.log(ranNum);
 
+  //assigns random value for each jewel when clicked
+  var purple1 = Math.floor(Math.random() * 12) + 1;
+  var green2 = Math.floor(Math.random() * 12) + 1;
+  var yellow3 = Math.floor(Math.random() * 12) + 1;
+  var red4 = Math.floor(Math.random() * 12) + 1;
+  console.log(purple1, green2, yellow3, red4);
 
-  //assign vrandom alue for each jewel
 
-  $(".purple1").on("click", function(){
-    var purple1 = Math.floor(Math.random() * 12 + 1);
-     console.log(purple1);
+  $(".pics").on("click", function () {
+    console.log($(this).attr("ranNum"));
   })
+
+//-------Get writers block in that I dont know what to do first or how things go together. 
+//-------I still have a hard time figuring out what functions/methods to use when and where----
+
+
+  // newGame();
+	
+
+		// // listen for clicks on any of the gems by targeting the gem class
+		// $(".pics").on("click", function() {
+		// 	// each gem has a value attribute of gem1, gem2, gem3, or gem 4
+		// 	// use this attribute to identify which gem the user actually clicked
+		// 	var pressed = $(this).attr("value");
+	  //       console.log(pressed);
+	  //       // add the value of the gem to the user's ongoing score tally
+	  //       if (pressed == "gem1") {
+	  //       	userScore += purple1;
+	  //       } else if (pressed == "gem2") {
+	  //       	userScore += green2;
+	  //       } else if (pressed == "gem3") {
+	  //       	userScore += yellow3;
+	  //       } else if (pressed == "gem4") {
+	  //       	userScore += red4;
+	  //       } else {
+	  //       	console.log("error");
+	  //       }
+	  //       // then update the html for the user score
+	  //       $(".scoreDiv").html(userScore);
+	  //       // call the function to see if user has won or lost
+	  //       // hasUserWonOrLost();
+		// });
+	
+
+	
+
+
+  // $(".purple1").on("click", function(){
+  //   var purple1 = Math.floor(Math.random() * 12 + 1);
+  //    console.log(purple1);
+  // })
   
-  $(".green2").on("click", function(){
-    var green2 = Math.floor(Math.random() * 12 + 1);
-      console.log(green2);
-  })
+  // $(".green2").on("click", function(){
+  //   var green2 = Math.floor(Math.random() * 12 + 1);
+  //     console.log(green2);
+  // })
     
-  $(".yellow3").on("click", function(){
-    var yellow3 = Math.floor(Math.random() * 12 + 1);
-      console.log(yellow3);
-  })
+  // $(".yellow3").on("click", function(){
+  //   var yellow3 = Math.floor(Math.random() * 12 + 1);
+  //     console.log(yellow3);
+  // })
 
-  $(".red4").on("click", function(){
-    var red4 = Math.floor(Math.random() * 12 + 1);
-      console.log(red4);
-  })
-
-
+  // $(".red4").on("click", function(){
+  //   var red4 = Math.floor(Math.random() * 12 + 1);
+  //     console.log(red4);
+  // })
 
 
+  //Count numbers and then Log wins and losses
+  
+  // $(".purple1").on("click", function() {
+    //assign a random number and use it throughout game
+    // if( )
+    // total++;
+    // console.log("purple1" + crystals[0] + " Total is " + total);
+    // $(".compRandom").text(total);
+  // })
+  // playgame();
 
+// ------------------------
 
 // $( document ).ready(function newGame() {
 
@@ -87,29 +191,3 @@ var max = 120;
 
 
 
-
-// ---------------------------
-
-// var wins = 0;
-// var losses = 0;
-// var sum = 0;
-// var matchNum = 1;
-// // note: in crystal array [0] is amethyst, [1] is blue, [2] is quartz, [3] is ruby 
-// var crystalNum = [0, 0, 0, 0];
-
-// function startNewGame() {
-//   // reset sum to 0 and display 0 for user
-//   sum = 0;
-//   $("#sum").text(sum);
-//   // Computer assigns a random number between 19 and 120 inclusive that the user will try to match
-//   matchNum = Math.floor(Math.random() * 102) + 19;
-//   console.log(matchNum);
-//   $("#number-to-match").text(matchNum);
-
-//   // Computer assigns different random numbers between 1 and 12 inclusive to each of the crystals 
-//   for (var i = 0; i < 4; i++) {
-//     crystalNum[i] = Math.floor(Math.random() * 12) + 1;
-//   }
-//   console.log(crystalNum);
-
-// }
